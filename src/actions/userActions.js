bff7f2905d0c1d887f0ad08bc0b11c56cfdcf350
ananthjos <68706680+ananthjos@users.noreply.github.com>
2022-04-26 10:33:33 +0530

    dummy commit

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -2,14 +2,14 @@ import {REGISTER_USER,LOGIN_USER,LOGOUT,SAVE_TEST} from './types';
 
 const url = "http://localhost:4000";
 // API call to register a user
-// export const  registerUser = (user)=>async(dispatch)=>{
-//    let response = await fetch(`${url}/users`,{method:'POST',headers:{
-//      'Content-Type':'application/json',body:JSON.stringify(user)
-//    }})
+export const  registerUser = (user)=>async(dispatch)=>{
+   let response = await fetch(`${url}/users`,{method:'POST',headers:{
+     'Content-Type':'application/json',body:JSON.stringify(user)
+   }})
 
-//    let data = await response.json();
-//    dispatch({type:REGISTER_USER,payload:data});
-// }
+   let data = await response.json();
+   dispatch({type:REGISTER_USER,payload:data});
+}
 
 // API call to login a user
 export const loginUser = (userId) => async (dispatch) => {
